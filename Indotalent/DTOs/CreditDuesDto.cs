13a881e8f122ffe28648c105cb82f059d36f2e57
Alejandro Espinoza Torrico <alejandro.espinoza.60092@usalesiana.edu.bo>
2024-07-15 14:08:03 -0400

    [@AlejandroEspinoza]: add credit dues  implementation

---
+++ Indotalent/DTOs/CreditDuesDto.cs
@@ -0,0 +1,20 @@
+using System;
+using Indotalent.Models.Enums;
+
+namespace Indotalent.DTOs
+{
+    public class CreditDuesDto
+    {
+        public int Id { get; set; }
+        public int CreditId { get; set; }
+        public string? FileImage { get; set; }
+        public DateTime InitialDate { get; set; }
+        public DateTime FinalDate { get; set; }
+        public int CustomerId { get; set; }
+        public int DueValue { get; set; }
+        public int DueNumber { get; set; }
+        public DateTime DueLapse { get; set; }
+        public int InitialPaymentPercentage { get; set; }
+        public PaymentStatus PaymentStatus { get; set; }
+    }
+}
---
+++ Indotalent/Models/Configurations/CreditDuesConfiguration.cs
@@ -0,0 +1,25 @@
+using Indotalent.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Indotalent.Models.Configurations
+{
+    public class CreditDuesConfiguration : _BaseConfiguration<CreditDues>
+    {
+        public override void Configure(EntityTypeBuilder<CreditDues> builder)
+        {
+            base.Configure(builder);
+
+            builder.Property(c => c.CreditId).IsRequired();
+            builder.Property(c => c.FileImage).HasMaxLength(255);
+            builder.Property(c => c.InitialDate).IsRequired();
+            builder.Property(c => c.FinalDate).IsRequired();
+            builder.Property(c => c.CustomerId).IsRequired();
+            builder.Property(c => c.DueValue).IsRequired();
+            builder.Property(c => c.DueNumber).IsRequired();
+            builder.Property(c => c.DueLapse).IsRequired();
+            builder.Property(c => c.InitialPaymentPercentage).IsRequired();
+            builder.Property(c => c.PaymentStatus).IsRequired();
+        }
+    }
+}
