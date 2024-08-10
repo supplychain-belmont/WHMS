ea476f100b78328ee05c7c2df4dd19180fb6666f
Alejandro Espinoza Torrico <alejandro.espinoza.60092@usalesiana.edu.bo>
2024-08-10 07:57:57 -0400

    [@AlejandroEspinoza]: add sales return controller update with mappers

--- Indotalent/ApiOData/SalesReturnController.cs
+++ Indotalent/ApiOData/SalesReturnController.cs
@@ -1,45 +1,66 @@
-﻿using Indotalent.Applications.SalesReturns;
+﻿using AutoMapper;
+using Indotalent.Applications.SalesReturns;
 using Indotalent.DTOs;
-
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.OData.Query;
+using Microsoft.AspNetCore.OData.Results;
 using Microsoft.AspNetCore.OData.Routing.Controllers;
-using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.OData.Formatter;
 
 namespace Indotalent.ApiOData
 {
     public class SalesReturnController : ODataController
     {
         private readonly SalesReturnService _salesReturnService;
+        private readonly IMapper _mapper;
 
-        public SalesReturnController(SalesReturnService salesReturnService)
+        public SalesReturnController(SalesReturnService salesReturnService, IMapper mapper)
         {
             _salesReturnService = salesReturnService;
+            _mapper = mapper;
         }
 
         [EnableQuery]
         public IQueryable<SalesReturnDto> Get()
         {
-            return _salesReturnService
-                .GetAll()
-                .Include(x => x.DeliveryOrder)
-                    .ThenInclude(x => x!.SalesOrder)
-                        .ThenInclude(x => x!.Customer)
-                .Select(rec => new SalesReturnDto
-                {
-                    Id = rec.Id,
-                    Number = rec.Number,
-                    ReturnDate = rec.ReturnDate,
-                    Status = rec.Status,
-                    DeliveryOrder = rec.DeliveryOrder!.Number,
-                    DeliveryDate = rec.DeliveryOrder!.DeliveryDate,
-                    Customer = rec.DeliveryOrder!.SalesOrder!.Customer!.Name,
-                    RowGuid = rec.RowGuid,
-                    CreatedAtUtc = rec.CreatedAtUtc,
-                });
+            return _salesReturnService.GetAllDtos();
         }
 
+        [EnableQuery]
+        [HttpGet("{key}")]
+        public async Task<SingleResult<SalesReturnDto>> Get([FromODataUri] int key)
+        {
+            var result = await _salesReturnService.GetDtoByIdAsync(key);
+            return SingleResult.Create(result != null ? new[] { result }.AsQueryable() : Enumerable.Empty<SalesReturnDto>().AsQueryable());
+        }
 
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] SalesReturnDto salesReturnDto)
+        {
+            var result = await _salesReturnService.CreateAsync(salesReturnDto);
+            return Created(result);
+        }
 
+        [HttpPut("{key}")]
+        public async Task<IActionResult> Put([FromODataUri] int key, [FromBody] SalesReturnDto salesReturnDto)
+        {
+            if (key != salesReturnDto.Id)
+            {
+                return BadRequest();
+            }
 
+            var result = await _salesReturnService.UpdateAsync(salesReturnDto);
+            return Updated(result);
+        }
+
+        [HttpDelete("{key}")]
+        public async Task<IActionResult> Delete([FromODataUri] int key)
+        {
+            await _salesReturnService.DeleteByIdAsync(key);
+            return NoContent();
+        }
     }
 }
--- Indotalent/Applications/SalesReturns/SalesReturnService.cs
+++ Indotalent/Applications/SalesReturns/SalesReturnService.cs
@@ -1,22 +1,74 @@
-﻿using Indotalent.Data;
+﻿using AutoMapper;
+using Indotalent.Data;
+using Indotalent.DTOs;
 using Indotalent.Infrastructures.Repositories;
 using Indotalent.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace Indotalent.Applications.SalesReturns
 {
     public class SalesReturnService : Repository<SalesReturn>
     {
+        private readonly IMapper _mapper;
+
         public SalesReturnService(
             ApplicationDbContext context,
             IHttpContextAccessor httpContextAccessor,
-            IAuditColumnTransformer auditColumnTransformer) :
+            IAuditColumnTransformer auditColumnTransformer,
+            IMapper mapper) :
                 base(
                     context,
                     httpContextAccessor,
                     auditColumnTransformer)
         {
+            _mapper = mapper;
         }
 
+        public IQueryable<SalesReturnDto> GetAllDtos()
+        {
+            return _mapper.ProjectTo<SalesReturnDto>(_context.Set<SalesReturn>()
+                .Include(x => x.DeliveryOrder)
+                    .ThenInclude(x => x!.SalesOrder)
+                        .ThenInclude(x => x!.Customer)
+                .AsQueryable());
+        }
 
+        public async Task<SalesReturnDto?> GetDtoByIdAsync(int id)
+        {
+            var salesReturn = await _context.Set<SalesReturn>()
+                .Include(x => x.DeliveryOrder)
+                    .ThenInclude(x => x!.SalesOrder)
+                        .ThenInclude(x => x!.Customer)
+                .FirstOrDefaultAsync(x => x.Id == id);
+
+            return _mapper.Map<SalesReturnDto>(salesReturn);
+        }
+
+        public async Task<SalesReturnDto> CreateAsync(SalesReturnDto salesReturnDto)
+        {
+            var salesReturn = _mapper.Map<SalesReturn>(salesReturnDto);
+            await AddAsync(salesReturn);
+            return _mapper.Map<SalesReturnDto>(salesReturn);
+        }
+
+        public async Task<SalesReturnDto> UpdateAsync(SalesReturnDto salesReturnDto)
+        {
+            var salesReturn = await _context.Set<SalesReturn>().FindAsync(salesReturnDto.Id);
+            if (salesReturn == null)
+            {
+                throw new Exception("SalesReturn not found.");
+            }
+
+            _mapper.Map(salesReturnDto, salesReturn);
+            await UpdateAsync(salesReturn);
+            return _mapper.Map<SalesReturnDto>(salesReturn);
+        }
+
+        public async Task DeleteByIdAsync(int id)
+        {
+            await base.DeleteByIdAsync(id);
+        }
     }
 }
