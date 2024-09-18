066ebb3ac89709d769c2ed9114cdf79c2d32be21
Alejandro Espinoza Torrico <alejandro.espinoza.60092@usalesiana.edu.bo>
2024-09-18 07:24:24 -0400

    [@AlejandroEspinozaT] : add CRUD updated operations for scrapping and products

--- Indotalent/ApiOData/ProductController.cs
+++ Indotalent/ApiOData/ProductController.cs
@@ -6,16 +6,28 @@ using Indotalent.DTOs;
 using Microsoft.AspNetCore.OData.Query;
 using Microsoft.AspNetCore.OData.Routing.Controllers;
 using Microsoft.EntityFrameworkCore;
+using Indotalent.Applications.Products;
+using Indotalent.DTOs;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.OData.Query;
+using Microsoft.AspNetCore.OData.Routing.Controllers;
+using Microsoft.EntityFrameworkCore;
+using AutoMapper;
+using Indotalent.Models.Entities;
+
+using Microsoft.AspNetCore.OData.Formatter;
 
 namespace Indotalent.ApiOData
 {
     public class ProductController : ODataController
     {
         private readonly ProductService _productService;
+        private readonly IMapper _mapper;
 
-        public ProductController(ProductService productService)
+        public ProductController(ProductService productService, IMapper mapper)
         {
             _productService = productService;
+            _mapper = mapper;
         }
 
         [EnableQuery]
@@ -37,6 +49,70 @@ namespace Indotalent.ApiOData
                     UnitPrice = rec.UnitPrice,
                     Physical = rec.Physical,
                 });
+        }
+        [EnableQuery]
+        [HttpGet("{key}")]
+        public async Task<ActionResult<ProductDto>> Get([FromODataUri] int key)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var product =  _productService.GetByIdAsync(key, x => x.ProductGroup, x => x.UnitMeasure);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            var dto = _mapper.Map<ProductDto>(product);
+            return Ok(dto);
+        }
+        public async Task<ActionResult<ProductDto>> Post([FromBody] ProductDto productDto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var product = _mapper.Map<Product>(productDto);
+            await _productService.AddAsync(product);
+            var createdDto = _mapper.Map<ProductDto>(product);
+
+            return Created(createdDto);
+        }
+        public async Task<ActionResult<ProductDto>> Put([FromRoute] int key, [FromBody] ProductDto productDto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var currentProduct = await _productService.GetByIdAsync(key);
+            if (currentProduct == null)
+            {
+                return NotFound();
+            }
+
+            if (currentProduct.Number != productDto.Number)
+            {
+                return BadRequest("Unable to update product number.");
+            }
+
+            _mapper.Map(productDto, currentProduct);
+            await _productService.UpdateAsync(currentProduct);
+            return NoContent();
+        }
+
+        public async Task<ActionResult> Delete([FromRoute] int key)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            await _productService.DeleteByIdAsync(key);
+            return NoContent();
         }
     }
 }
--- Indotalent/ApiOData/ProductGroupController.cs
+++ Indotalent/ApiOData/ProductGroupController.cs
@@ -1,18 +1,24 @@
 ﻿using Indotalent.Applications.ProductGroups;
 using Indotalent.DTOs;
-
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.OData.Query;
 using Microsoft.AspNetCore.OData.Routing.Controllers;
+using Microsoft.AspNetCore.OData.Formatter;
+using AutoMapper;
+using Indotalent.Models.Entities;
+using Microsoft.AspNetCore.JsonPatch;
 
 namespace Indotalent.ApiOData
 {
     public class ProductGroupController : ODataController
     {
         private readonly ProductGroupService _productGroupService;
+        private readonly IMapper _mapper;
 
-        public ProductGroupController(ProductGroupService productGroupService)
+        public ProductGroupController(ProductGroupService productGroupService, IMapper mapper)
         {
             _productGroupService = productGroupService;
+            _mapper = mapper;
         }
 
         [EnableQuery]
@@ -29,7 +35,92 @@ namespace Indotalent.ApiOData
                     CreatedAtUtc = rec.CreatedAtUtc
                 });
         }
+         [EnableQuery]
+        [HttpGet("{key}")]
+        public async Task<ActionResult<ProductGroupDto>> Get([FromODataUri] int key)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
 
+            var productGroup = await _productGroupService.GetByIdAsync(key);
+            if (productGroup == null)
+            {
+                return NotFound();
+            }
 
+            var dto = _mapper.Map<ProductGroupDto>(productGroup);
+            return Ok(dto);
+        }
+
+        public async Task<ActionResult<ProductGroupDto>> Post([FromBody] ProductGroupDto productGroupDto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var productGroup = _mapper.Map<ProductGroup>(productGroupDto);
+            await _productGroupService.AddAsync(productGroup);
+            var createdDto = _mapper.Map<ProductGroupDto>(productGroup);
+
+            return Created(createdDto);
+        }
+
+        public async Task<ActionResult<ProductGroupDto>> Put([FromRoute] int key, [FromBody] ProductGroupDto productGroupDto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var currentProductGroup = await _productGroupService.GetByIdAsync(key);
+            if (currentProductGroup == null)
+            {
+                return NotFound();
+            }
+
+            _mapper.Map(productGroupDto, currentProductGroup);
+            await _productGroupService.UpdateAsync(currentProductGroup);
+            return NoContent();
+        }
+
+        public async Task<ActionResult> Patch([FromRoute] int key, [FromBody] JsonPatchDocument<ProductGroupDto> patchDto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var currentProductGroup = await _productGroupService.GetByIdAsync(key);
+            if (currentProductGroup == null)
+            {
+                return NotFound();
+            }
+
+            var productGroupDto = _mapper.Map<ProductGroupDto>(currentProductGroup);
+            patchDto.ApplyTo(productGroupDto, ModelState);
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _mapper.Map(productGroupDto, currentProductGroup);
+            await _productGroupService.UpdateAsync(currentProductGroup);
+            return NoContent();
+        }
+
+        public async Task<ActionResult> Delete([FromRoute] int key)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            await _productGroupService.DeleteByIdAsync(key);
+            return NoContent();
+        }
     }
 }
