b4791f65bbccbd9758011435ffa3f7d33ecab514
Santiago Huanacu Sanchez <santihs.sanchez.dev@gmail.com>
2024-10-04 02:52:21 -0400

    [@santihsdev] Update entities to use decimals

--- Indotalent/Models/Entities/SalesOrderItem.cs
+++ Indotalent/Models/Entities/SalesOrderItem.cs
@@ -12,9 +12,9 @@ namespace Indotalent.Models.Entities
         public required int ProductId { get; set; }
         public Product? Product { get; set; }
         public string? Summary { get; set; }
-        public double? UnitPrice { get; set; } = 0;
-        public double? Quantity { get; set; } = 1;
-        public double? Total { get; set; } = 0;
+        public decimal UnitPrice { get; set; } = 0;
+        public decimal Quantity { get; set; } = 1;
+        public decimal? Total { get; set; } = 0;
 
         public void RecalculateTotal()
         {
